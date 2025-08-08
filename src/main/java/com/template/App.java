adb71cf134bb9caf3f7af79a1b3930ce0266cc8c
Tomasgarciabado <tgarciabado@mail.austral.edu.ar>
2025-08-08 12:35:41 -0300

    primer entrega

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -4,61 +4,74 @@ public class App {
 
     // 1. Sum of Two Integers
     public int sum(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        return a + b;
     }
 
     // 2. Check Even or Odd
     public boolean isEven(int number) {
-        // TODO: Implement this method
-        return false;
+        return number % 2 == 0;
     }
 
     // 3. Maximum of Two Numbers
     public int max(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        return (a > b) ? a : b;
     }
 
     // 4. Factorial of a Number
     public int factorial(int n) {
-        // TODO: Implement this method
-        return 0;
+        if (n < 0) return -1;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 
     // 5. Count Characters in a String
     public int countChars(String input) {
-        // TODO: Implement this method
-        return 0;
+        if (input == null) return 0;
+        return input.length();
     }
 
     // 6. Reverse a String
     public String reverse(String input) {
-        // TODO: Implement this method
-        return "";
+        if (input == null) return "";
+        return new StringBuilder(input).reverse().toString();
     }
 
     // 7. Check Prime Number
     public boolean isPrime(int number) {
-        // TODO: Implement this method
-        return false;
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
     }
 
     // 8. Find the Smallest Element in an Array
     public int findMin(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        if (array == null || array.length == 0) return 0;
+        int min = array[0];
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
     }
 
     // 9. Sum of Elements in an Array
     public int arraySum(int[] array) {
-        // TODO: Implement this method
-        return 0;
+        if (array == null) return 0;
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum;
     }
 
     // 10. Convert Celsius to Fahrenheit
     public double celsiusToFahrenheit(double celsius) {
-        // TODO: Implement this method
-        return 0.0;
+        return (celsius * 9 / 5) + 32;
     }
 }
--- src/test/java/com/template/AppTest.java
+++ src/test/java/com/template/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class AppTest {
+class wAppTest {
 
     App exercises = new App();
 
